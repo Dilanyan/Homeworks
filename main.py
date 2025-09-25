36dd353b86a560d154e8c47975c2282dbbf0a1ee
Dilanyan <dilanyan.n@gmail.com>
2025-09-25 16:28:37 +0400

    selenium homework 1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from selenium import webdriver
+from selenium_helper import Selenium_helper
+
+drivers = [webdriver.Firefox(), webdriver.Chrome(), webdriver.Edge()]
+for driver in drivers:
+    Selenium_helper.do_some_rutin(driver)
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
