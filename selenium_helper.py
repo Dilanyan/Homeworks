8c42a457ff0b8e3fcaefea4fef38eef4c19e9f4e
Dilanyan <dilanyan.n@gmail.com>
2025-09-28 15:13:27 +0400

    selenium homework 3 additional

--- selenium_helper.py
+++ selenium_helper.py
@@ -1,5 +1,8 @@
 from time import sleep
 import pytest
+from selenium.webdriver.common.by import By
+
+
 class Selenium_helper():
     """
         In this class collected all methods that will help to do something related to the selenium part of our project, for example:
@@ -28,3 +31,28 @@ class Selenium_helper():
             driver.minimize_window()
         finally:
             driver.quit()
+
+
+
+    @staticmethod
+    def button_click(driver, button_name):
+        button = driver.find_element(By.XPATH, f"//button[text()='{button_name}']")
+        button.click()
+        sleep(5)
+
+    @staticmethod
+    def message_assertion(driver, message_text):
+        p_element = driver.find_element(By.CSS_SELECTOR, "p[id='message']")
+        assert p_element.is_displayed()
+        assert p_element.text in message_text
+
+    @staticmethod
+    def click_checkbox(driver):
+        checkbox = driver.find_element(By.CSS_SELECTOR, "input[type='checkbox']")
+        checkbox.click()
+
+    @staticmethod
+    def type_text_in_text_field(driver, text):
+        input_text_field = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
+        input_text_field.send_keys(text)
+
